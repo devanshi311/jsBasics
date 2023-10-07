e0b095d4af47fe30f28b50801b350bc1fdffe337
devanshi311 <104788876+devanshi311@users.noreply.github.com>
2023-10-07 20:38:48 +0000

    commit

---
+++ 01.js
@@ -0,0 +1,18 @@
+const number = 223;
+let cutomerName = "Devanshi";
+var customerEmail = "sdf@gmail.com"
+customerCity = "Ahmedabad"
+const customerId = 24;
+/*
+    number = 4;
+DO NOT TRY TO CHANGE CONST THING IT NEVER DOES
+*/
+cutomerName = "Devanshi Godhaniya"
+customerEmail = "addd@gamil.com"
+customerCity = "Porbander"
+/**
+var customerEmail = "sdf@gmail.com"
+customerCity = "Ahmedabad"
+ * AND ALSO DO NOT TRY ABOVE METHOD THEY ARE NOT OKAY AT BLOCK LEVEL THING
+ */
+console.table([customerId, number, cutomerName, customerEmail, customerCity])
