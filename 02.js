1ac5259db36e5c7050a986261d526d72a5904864
devanshi311 <104788876+devanshi311@users.noreply.github.com>
2023-10-07 21:31:42 +0000

    commit

---
+++ 02.js
@@ -0,0 +1,16 @@
+// Data-types
+// Data-types
+/**
+ * number =>
+ * bigInt
+ * boolean
+ * null
+ * undefined
+ * string
+ * symbol
+ */
+let a = "devanshi";
+let b = 34;
+let c = false;
+let d;
+console.log(typeof c)
---
+++ 03.js
@@ -0,0 +1,22 @@
+let score = "33"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueNum = Number(score)
+console.log(typeof valueNum)
+console.log(valueNum)
+
+
+let isLoggedIn = " "
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+
+
+
+let someNumber = 33
+let stringOfNumber = String(someNumber)
+console.log(typeof stringOfNumber)
+console.log(stringOfNumber)
